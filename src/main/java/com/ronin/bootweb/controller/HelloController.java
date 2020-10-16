b96621409b4d1bff3ffad687a9277a89010264b4
Joe Morrogh <roninjoe@gmail.com>
2020-10-16 12:49:04 -0400

    initial swagger

--- src/main/java/com/ronin/bootweb/controller/HelloController.java
+++ src/main/java/com/ronin/bootweb/controller/HelloController.java
@@ -7,12 +7,20 @@ import com.ronin.bootweb.service.HelloService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 @RestController
+@Api("/api")
 public class HelloController {
 
         @Resource
         private HelloService helloService;
         
+        @ApiOperation(
+                value = "Say Hello",
+                notes="This is a simple demo endpoint to say hello to whatever name is passed in"
+        )
         @GetMapping("/hello")
         public String hello(@RequestParam(value = "name", defaultValue="there") String name) {
                 return helloService.sayHello(name);
