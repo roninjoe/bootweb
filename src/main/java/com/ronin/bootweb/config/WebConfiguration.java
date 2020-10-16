0d64da9d54948c6130157890fe5dc6129df155a1
Joe Morrogh <roninjoe@gmail.com>
2020-10-16 13:44:43 -0400

    add EnableSwagger2 annotation to enable swagger

--- src/main/java/com/ronin/bootweb/config/WebConfiguration.java
+++ src/main/java/com/ronin/bootweb/config/WebConfiguration.java
@@ -4,7 +4,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @Configuration
+@EnableSwagger2
 public class WebConfiguration implements WebMvcConfigurer {
     @Override 
     public void addViewControllers(ViewControllerRegistry registry) {
