03a31ddec93a54aeb7bcb7693afe08522185b947
Joe Morrogh <roninjoe@gmail.com>
2020-10-16 12:26:41 -0400

    add Service stereotype annotation

--- src/main/java/com/ronin/bootweb/service/HelloService.java
+++ src/main/java/com/ronin/bootweb/service/HelloService.java
@@ -1,10 +1,12 @@
 package com.ronin.bootweb.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class HelloService {
 
 	public String sayHello(String name) {
                        return String.format("Hello %s!",name);
 		}
-
 }
 
